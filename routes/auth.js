caf2dc49a52fde93640785bf292563dc7c8956b2
Emin Ahmadov <emin.ahmadov99@gmail.com>
2020-02-28 12:37:35 +0100

    Added encryption logic for password

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,5 @@
 const router = require('express').Router()
+const bcrypt = require('bcryptjs')
 const User = require('../model/User')
 const { registrationValidation } = require('../validation')
 
@@ -15,16 +16,20 @@ router.post('/register', async (req, res) => {
     return res.status(400).send('Email already exists')
   }
 
+  // Hashing the password
+  const salt = await bcrypt.genSalt(10)
+  const hashedPassword = await bcrypt.hash(req.body.password, salt)
+
   // Creation of a new user
   const user = new User({
     name: req.body.name,
     email: req.body.email,
-    password: req.body.password
+    password: hashedPassword
   })
 
   try {
-    const savedUser = await user.save()
-    res.send(savedUser)
+    await user.save()
+    res.send({ user: user._id })
   } catch (err) {
     res.status(400).send(err)
   }
